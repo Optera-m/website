55166c4eb1af16235bc2f36991de86748922c49f
Optera-m <maarten@optera.be>
2025-01-03 01:31:24 +0100

    removed gm

--- js/custom.js
+++ js/custom.js
@@ -30,16 +30,6 @@ $(".owl-carousel").owlCarousel({
     }
 });
 
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
