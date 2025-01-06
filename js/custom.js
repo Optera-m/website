c6424758ad722784363baa09f9388840971c6db7
Optera-m <maarten@optera.be>
2025-01-06 17:32:08 +0100

    new animation nav hamburger

--- js/custom.js
+++ js/custom.js
@@ -5,12 +5,12 @@ function getCurrentYear() {
     document.querySelector("#displayDateYear").innerText = year;
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
